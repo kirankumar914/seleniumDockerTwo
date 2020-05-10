72900097f4fcfb04afca35e69f87dbc6867c9b0d
SG0224767 <kirankumar.repaka@sabre.com>
2020-05-10 17:59:12 +0530

    adding sh file once agin with changes.

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,12 +1,6 @@
-#!/usr/bin/env bash
-# Environment Variables
-# HUB_HOST
-# BROWSER
-# MODULE
-
 echo "Checking if hub is ready - $HUB_HOST"
 
-while [ "$( curl -s http://$HUB_HOST:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
+while [ "$( curl -s http://$HUB_HOST:4444/wd/hub/status | jq -r .value.ready )" != "true" ];
 do
 	sleep 1
 done
